810e9445cf147feeaeea6a21b50f66a47db13a80
Imma V <imma.villaplana@iesalvarofalomir.org>
2021-12-02 18:09:32 +0100

    Pide una temperatura e indica si es negativa o positiva

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -4,6 +4,8 @@
  */
 package javaapplication2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author immque
@@ -14,8 +16,19 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner lector = new Scanner(System.in);
+        float temperatura;
+        
         System.out.println("Welcome!");
+        
+        System.out.print("Indica la temperatura: ");
+        temperatura = lector.nextFloat();
+        
+        if (temperatura < 0) {
+            System.out.println(temperatura + " grados es una temperatura negativa");
+        }
+        else {
+            System.out.println(temperatura + " grados es una temperatura positiva");
+        }
     }
-    
 }
